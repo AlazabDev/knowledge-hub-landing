11d15aac02400e3d4b1da2f3eb51eb7557434866
Alazab Developer <mohamed@alazab.com>
2026-05-12 17:44:48 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { KnowledgeBase } from "@/components/KnowledgeBase";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
@@ -16,7 +17,14 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          
+          {/* مسار قاعدة المعرفة */}
+          <Route path="/kb" element={<KnowledgeBase />} />
+          <Route path="/knowledge-base" element={<KnowledgeBase />} />
+          
+          {/* مسارات إضافية يمكنك إضافتها هنا */}
+          
+          {/* مسار catch-all للصفحات غير الموجودة */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
